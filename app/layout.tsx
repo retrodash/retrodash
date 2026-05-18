e5c65af4749e56a05b3b88ee58a9c1f508998c4c
Igor-Ponticelli <patrickigor.ip@gmail.com>
2026-05-18 17:23:32 -0300

    revert: remove Lenis, restore CSS smooth scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
-import SmoothScroll from "@/components/SmoothScroll";
 import "./globals.css";
 
 const jakarta = Plus_Jakarta_Sans({
@@ -22,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${jakarta.variable} h-full`} suppressHydrationWarning>
       <body className="min-h-full flex flex-col font-sans antialiased">
-        <SmoothScroll>{children}</SmoothScroll>
+        {children}
       </body>
     </html>
   );
