525350d2f1fcfc9a038a994863af8c8b2c2f6af6
Igor-Ponticelli <patrickigor.ip@gmail.com>
2026-05-23 17:34:51 -0300

    fix(theme): move ThemeProvider into locale layout body

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
-import { ThemeProvider } from "@/components/ui/ThemeProvider";
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return children;
 }
